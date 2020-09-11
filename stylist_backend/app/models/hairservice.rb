d6b644de616a7342cb17e94f31603ead253550aa
SWFullPotential <swfullpotential@gmail.com>
2020-09-11 14:03:07 -0600

    add validations

--- stylist_backend/app/models/hairservice.rb
+++ stylist_backend/app/models/hairservice.rb
@@ -1,3 +1,5 @@
 class Hairservice < ApplicationRecord
     belongs_to :hairstylist
+    validates :serviceName, presence: true 
+    validates :price, precnese: true
 end
--- stylist_backend/app/models/hairstylist.rb
+++ stylist_backend/app/models/hairstylist.rb
@@ -1,3 +1,5 @@
 class Hairstylist < ApplicationRecord
     has_many :hairservices
+    validates :name, presence: true 
+    validates :year_licensed, presence: true
 end
