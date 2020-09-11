9bfbe53130056e57c35ef6d005973e7164e33de0
SWFullPotential <swfullpotential@gmail.com>
2020-09-11 15:32:35 -0600

    added URLS

---
+++ stylist_frontend/src/index.js
@@ -0,0 +1,9 @@
+/*
+    add URLs, fetch data, create cards to populate the stylist and services in. 
+
+*/
+const BASE_URL = "http://localhost:3000"
+const HAIRSTYLIST_URL = `${BASE_URL}/hairstylists`
+const SERVICES_URL = `${BASE_URL}/hairservices`
+
+
