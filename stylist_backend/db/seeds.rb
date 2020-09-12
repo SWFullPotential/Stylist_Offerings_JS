92b2a2be7bac60ebcb21cb15ddeb36c91548bf18
SWFullPotential <swfullpotential@gmail.com>
2020-09-11 22:13:07 -0600

    removed notes

--- stylist_backend/db/seeds.rb
+++ stylist_backend/db/seeds.rb
@@ -1,12 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
-Hairstylist.create(name: "Samantha", year_licensed: 2013)
+# Hairstylist.create(name: "Samantha", year_licensed: 2013)
 
 Hairservice.create(service_name: "haircut", price: 45, hairstylist_id: 1)
 Hairservice.create(service_name: "blow-out", price: 25, hairstylist_id: 1)
