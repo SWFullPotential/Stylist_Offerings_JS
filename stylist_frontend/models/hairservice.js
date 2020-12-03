9cdac1222d06643232ac160bed592e223b2c06be
SWFullPotential <swfullpotential@gmail.com>
2020-12-02 19:44:45 -0700

    edit typo

--- stylist_frontend/models/hairservice.js
+++ stylist_frontend/models/hairservice.js
@@ -1,23 +1,27 @@
 class Hairservice {
-    static all = [];
-    constructor(id, service_name, price, hairstylist_id){
-        this.id = id;
-        this.service_name = service_name;
-        this.price = price;
-        this.hairstylist_id = hairstylist_id;
-    }
+  static all = [];
+  constructor(id, service_name, price, hairstylist_id) {
+    this.id = id;
+    this.service_name = service_name;
+    this.price = price;
+    this.hairstylist_id = hairstylist_id;
+  }
 
-    static create(id, service_name, price, hairstylist_id){
-        let service = new Hairservice(id, service_name, price, hairstylist_id)
-            Hairservice.all.push(service)
-            return service
-    }
-    
-    removeService() {
-        let hairstylist = Hairstylist.all.find(stylist => stylist.id == this.hairstylist_id)
-        hairstylist.hairservices = hairstylist.hairservices.filter(service => service.id != this.id)
-        Hairservice.all = Hairservice.all.filter(service => service.id != this.id)
+  static create(id, service_name, price, hairstylist_id) {
+    let service = new Hairservice(id, service_name, price, hairstylist_id);
+    Hairservice.all.push(service);
+    return service;
+  }
 
-    }
-
-}ß
+  removeService() {
+    let hairstylist = Hairstylist.all.find(
+      (stylist) => stylist.id == this.hairstylist_id
+    );
+    hairstylist.hairservices = hairstylist.hairservices.filter(
+      (service) => service.id != this.id
+    );
+    Hairservice.all = Hairservice.all.filter(
+      (service) => service.id != this.id
+    );
+  }
+}
